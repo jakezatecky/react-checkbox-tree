b6715c025de25e29c58e4ba8e46f6f2b2bb10743
Jake Zatecky <jake.zatecky@gmail.com>
2017-01-14 18:38:12 -0500

    Do not mutate properties
    Resolves #12

--- src/js/Tree.js
+++ src/js/Tree.js
@@ -39,7 +39,7 @@ class Tree extends React.Component {
 	}
 
 	onCheck(node) {
-		const { checked } = this.props;
+		const checked = [...this.props.checked];
 		const isChecked = node.checked;
 
 		this.setCheckState(checked, node, isChecked);
@@ -49,7 +49,7 @@ class Tree extends React.Component {
 
 	onExpand(node) {
 		const isExpanded = node.expanded;
-		const expanded = this.props.expanded;
+		const expanded = [...this.props.expanded];
 		const nodeIndex = expanded.indexOf(node.value);
 
 		if (!isExpanded && nodeIndex > -1) {
