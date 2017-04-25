dba591e00d1641fe8e53c03dca7e2f87f6344609
Jake Zatecky <jake.zatecky@gmail.com>
2017-04-24 22:45:10 -0400

    Create example to test out a large number of nodes
    References #14.

--- examples/src/index.js
+++ examples/src/index.js
@@ -4,7 +4,9 @@ import ReactDOM from 'react-dom';
 import BasicExample from './js/BasicExample';
 import CustomIconsExample from './js/CustomIconsExample';
 import PessimisticToggleExample from './js/PessimisticToggleExample';
+import LargeDataExample from './js/LargeDataExample';
 
 ReactDOM.render(<BasicExample />, document.getElementById('basic-example'));
 ReactDOM.render(<CustomIconsExample />, document.getElementById('custom-icons-example'));
 ReactDOM.render(<PessimisticToggleExample />, document.getElementById('pessimistic-toggle-example'));
+ReactDOM.render(<LargeDataExample />, document.getElementById('large-data-example'));
