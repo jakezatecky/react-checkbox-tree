93afb5ddb29927b14549af27971f5c1fbba21c12
Jake Zatecky <jake.zatecky@gmail.com>
2019-01-28 07:26:56 -0500

    Use React.ReactNode over JSX.Element for `label` property
    Resolves #127.

--- src/index.d.ts
+++ src/index.d.ts
@@ -2,7 +2,7 @@ import * as React from 'react';
 
 declare module "react-checkbox-tree" {
     interface Node {
-        label: JSX.Element;
+        label: React.ReactNode;
         value: string;
         children?: Array<Node>;
         className?: string;
