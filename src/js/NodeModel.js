7c38686576ffa294eeeb570ad335d5ab705c3b4c
Guy Elsmore-Paddock <guy.paddock@gmail.com>
2019-11-26 18:57:36 -0500

    [#171] Allow Parent Nodes to Have an Empty `children` Array (#177)
    In computer science terms, a node without any children is a "leaf". But
    in practical use, it's common to run into structures (e.g. empty folders
    in file systems) where a node is still a parent because it does not
    _yet_ have children but has the capacity to have children in the future.
    
    Closes #171.

--- src/js/NodeModel.js
+++ src/js/NodeModel.js
@@ -52,7 +52,7 @@ class NodeModel {
     }
 
     nodeHasChildren(node) {
-        return Array.isArray(node.children) && node.children.length > 0;
+        return Array.isArray(node.children);
     }
 
     getDisabledState(node, parent, disabledProp, noCascade) {
