6a68d46d78a3c21ee8f64580c69e6c4eb9b9e75f
Jake Zatecky <jake.zatecky@gmail.com>
2023-02-15 10:32:24 -0500

    Simplify underlying Button component

--- src/js/Button.js
+++ src/js/Button.js
@@ -1,30 +1,28 @@
 import PropTypes from 'prop-types';
 import React from 'react';
 
-class Button extends React.PureComponent {
-    static propTypes = {
-        children: PropTypes.node.isRequired,
-        title: PropTypes.string,
-    };
+const propTypes = {
+    children: PropTypes.node.isRequired,
+    title: PropTypes.string,
+};
+const defaultProps = {
+    title: null,
+};
 
-    static defaultProps = {
-        title: null,
-    };
-
-    render() {
-        const { children, title, ...props } = this.props;
-
-        return (
-            <button
-                aria-label={title}
-                title={title}
-                type="button"
-                {...props}
-            >
-                {children}
-            </button>
-        );
-    }
+function Button({ children, title, ...props }) {
+    return (
+        <button
+            aria-label={title}
+            title={title}
+            type="button"
+            {...props}
+        >
+            {children}
+        </button>
+    );
 }
+
+Button.propTypes = propTypes;
+Button.defaultProps = defaultProps;
 
 export default Button;
